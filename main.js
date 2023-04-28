e9efb4e10945b53e3b806687c1f3e24a74e9ad67
t0mperi <97505632+t0mperi@users.noreply.github.com>
2023-04-28 21:43:37 +0300

    test

--- main.js
+++ main.js
@@ -3,34 +3,50 @@
 import kaboom from "kaboom"
 
 
-addLevel([
-    "                          $",
-    "                          $",
-    "           $$         =   $",
-    "  %      ====         =   $",
-    "                      =    ",
-    "       ^^      = >        &",
-    "===========================",
-], {
 
-    
-    // define the size of each block
-    width: 32,
-    height: 32,
-    // define what each symbol means, by a function returning a component list (what will be passed to add())
-    "=": () => [
-        sprite("floor"),
-        area(),
-        solid(),
-    ],
-    "$": () => [
-        sprite("coin"),
-        area(),
-        pos(0, -9),
-    ],
-    "^": () => [
-        sprite("spike"),
-        area(),
-        "danger",
-    ],
+const block_size = 20
+
+const directions = 
+{
+    UP: 'up',
+    DOWN: 'down',
+    LEFT: 'left',
+    RIGHT: 'right',
+}
+
+
+let current_direction = directions.RIGHT
+let run_action = false
+let snake_length = 4
+let snake_body = []
+
+
+const map = addlevel([
+      "xxxxxxxxxxxxxxxxxxxx",
+      "x                  x",
+      "x             x    x",
+      "x             x    x",
+      "x             x    x",
+      "x                  x",
+      "x                  x",
+      "x                  x",
+      "x                  x",
+      "x                  x",
+      "x                  x",
+      "x                  x",
+      "x       x          x",
+      "x       x          x",
+      "x       x          x",
+      "x                  x",
+      "x                  x",
+      "x                  x",
+      "x                  x",
+      "xxxxxxxxxxxxxxxxxxxx",
+],{
+
+
+      width:block_size,
+      height: block_size,
+      pos: vec2(0,0),
+      'x': () => [rect(block_size), color(255, 255, 255), area(), 'wall'],
 })
