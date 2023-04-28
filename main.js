94144bf4e8103fffc80fe40fbd2ebbf66164ad22
t0mperi <97505632+t0mperi@users.noreply.github.com>
2023-04-28 22:52:58 +0300

    test

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 
 import kaboom from "kaboom"
 
+kaboom();
 
-
-const block_size = 20
+const block_size = 5
 
 const directions = 
 {
@@ -20,8 +20,9 @@ let run_action = false
 let snake_length = 4
 let snake_body = []
 
-
-const map = addlevel([
+//levels
+const map = addLevel([
+  
       "xxxxxxxxxxxxxxxxxxxx",
       "x                  x",
       "x             x    x",
@@ -42,11 +43,32 @@ const map = addlevel([
       "x                  x",
       "x                  x",
       "xxxxxxxxxxxxxxxxxxxx",
-],{
-
-
-      width:block_size,
+], 
+{
+      width: block_size,
       height: block_size,
-      pos: vec2(0,0),
-      'x': () => [rect(block_size), color(255, 255, 255), area(), 'wall'],
+      pos: vec2(0, 0),
+      'x': () => [rect(block_size, block_size), color(255, 0, 0), area(), 'wall'],
+})
+
+//Snake move
+keyDown('up', () => {
+    if (current_direction !== directions.DOWN) {
+        current_direction = directions.UP
+    }
+})
+keyDown('down', () => {
+    if (current_direction !== directions.UP) {
+        current_direction = directions.DOWN
+    }
+})
+keyDown('left', () => {
+    if (current_direction !== directions.RIGHT) {
+        current_direction = directions.LEFT
+    }
+})
+keyDown('right', () => {
+    if (current_direction !== directions.LEFT) {
+        current_direction = directions.RIGHT
+    }
 })
